00ffbd05c20494a248709fa200ffde54e6b590dc
Adam Garberg <adamgarberg@gmail.com>
2022-01-15 15:28:36 -0600

    this will add 6 songs to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,19 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+let collection = [];    //create empty array
 
 function addToCollection(title, artist, yearPublished) {
   const name = {};
   name.title = title,
   name.artist = artist,
-  name.yearPublished = yearPublished,
+  name.yearPublished = yearPublished;
   collection.push(name);
-  return name
-};
+  };
+
+addToCollection('BootyShaker', 'SirBumpsAlot', 2026);
+addToCollection('Shambles', 'Millers', 2901);
+addToCollection('Party Pants', 'SirBumpsAlot', 2026 );
+addToCollection('Your Name Here', 'Atmosphere', 2011 );
+addToCollection('Satisfaction', 'Benny Benassi', 2005 );
+addToCollection('Crazy', 'Millers', 2901 );
+console.log(collection) // this console.log must be added to get any response?
