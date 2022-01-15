a81c8942a556d54809ef78e0cac50ae1eee8851e
Adam Garberg <adamgarberg@gmail.com>
2022-01-15 17:43:38 -0600

    Console.log each album as added using the returned value

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,12 +8,13 @@ function addToCollection(title, artist, yearPublished) {
   name.artist = artist,
   name.yearPublished = yearPublished;
   collection.push(name);
+  return name;
   };
 
-addToCollection('BootyShaker', 'SirBumpsAlot', 2026);
-addToCollection('Shambles', 'Millers', 2901);
-addToCollection('Party Pants', 'SirBumpsAlot', 2026 );
-addToCollection('Your Name Here', 'Atmosphere', 2011 );
-addToCollection('Satisfaction', 'Benny Benassi', 2005 );
-addToCollection('Crazy', 'Millers', 2901 );
-console.log(collection) // this console.log must be added to get any response?
+console.log(addToCollection('BootyShaker', 'SirBumpsAlot', 2026));
+console.log(addToCollection('Shambles', 'Millers', 2901));
+console.log(addToCollection('Party Pants', 'SirBumpsAlot', 2026 ));
+console.log(addToCollection('Your Name Here', 'Atmosphere', 2011 ));
+console.log(addToCollection('Satisfaction', 'Benny Benassi', 2005 ));
+console.log(addToCollection('Crazy', 'Millers', 2901 ));
+console.log(collection)
