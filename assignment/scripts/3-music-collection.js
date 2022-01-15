a45a3adaf2b19db4660b53c901595a911a737542
Adam Garberg <adamgarberg@gmail.com>
2022-01-15 17:44:05 -0600

    changed name to song

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,19 +2,25 @@ console.log('***** Music Collection *****')
 
 let collection = [];    //create empty array
 
-function addToCollection(title, artist, yearPublished) {
-  const name = {};
+function addToCollection(title, artist, yearPublished) { //function to create
+  const song = {};                                        //name object
   name.title = title,
   name.artist = artist,
   name.yearPublished = yearPublished;
   collection.push(name);
-  return name;
-  };
-
-console.log(addToCollection('BootyShaker', 'SirBumpsAlot', 2026));
+  return song;
+};
+// call function and add each song
+console.log(addToCollection('Booty Shaker', 'SirBumpsAlot', 2026));
 console.log(addToCollection('Shambles', 'Millers', 2901));
 console.log(addToCollection('Party Pants', 'SirBumpsAlot', 2026 ));
 console.log(addToCollection('Your Name Here', 'Atmosphere', 2011 ));
 console.log(addToCollection('Satisfaction', 'Benny Benassi', 2005 ));
 console.log(addToCollection('Crazy', 'Millers', 2901 ));
 console.log(collection)
+
+function showCollection
+
+
+
+function findByArtist
