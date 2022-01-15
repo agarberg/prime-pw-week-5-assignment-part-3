82c4dc313b3a3a34380539a3ebca6d4269dfea63
Adam Garberg <adamgarberg@gmail.com>
2022-01-15 11:54:29 -0600

    this adds collection array and function to add to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,12 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  const name = {};
+  name.title = title,
+  name.artist = artist,
+  name.yearPublished = yearPublished,
+  collection.push(name);
+  return name
+};
