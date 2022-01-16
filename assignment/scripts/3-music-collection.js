495c210a1145ec411c6fc97b19520fcc25d9ac68
Adam Garberg <adamgarberg@gmail.com>
2022-01-15 19:43:49 -0600

    this function will log title, artist and yearpublished

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,10 +4,10 @@ let collection = [];    //create empty array
 
 function addToCollection(title, artist, yearPublished) { //function to create
   const song = {};                                        //name object
-  name.title = title,
-  name.artist = artist,
-  name.yearPublished = yearPublished;
-  collection.push(name);
+  song.title = title,
+  song.artist = artist,
+  song.yearPublished = yearPublished;
+  collection.push(song);
   return song;
 };
 // call function and add each song
@@ -19,8 +19,26 @@ console.log(addToCollection('Satisfaction', 'Benny Benassi', 2005 ));
 console.log(addToCollection('Crazy', 'Millers', 2901 ));
 console.log(collection)
 
-function showCollection
+//////function using for loop
+function showCollection(array){
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++) {
+    console.log(`${array[i].title} by ${array[i].artist} published in ${array[i].yearPublished}`);
+  }
+}
+showCollection(collection)
+//end for loop function
 
+/////function using for OF loop
+function showCollection(array){
+  console.log(array.length);
+  for ( let i of array) {
+    console.log(`${i.title} by ${i.artist} published in ${i.yearPublished}`);
+  }
+}
+showCollection(collection)
+//end for OF loop function
 
+function findByArtist(){
 
-function findByArtist
+}
